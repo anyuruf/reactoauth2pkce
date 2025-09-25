72dff3727083e5774fa083da6809394fbb4bec48
Francis Anyuru <anyuruf@gmail.com>
2025-09-25 08:48:53 -0400

    Initial commit with react-oauth2-code-pkce package installation

---
+++ app/config/oauth2-config.tsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { AuthContext, type IAuthContext, type TAuthConfig } from 'react-oauth2-code-pkce';
+
+// Get info from http://localhost:8080/realms/test/.well-known/openid-configuration
+
+export const authConfig: TAuthConfig = {
+  clientId: 'suuku-frontend',
+  authorizationEndpoint: 'http://localhost:9080/realms/suuku-domain/protocol/openid-connect/auth',
+  logoutEndpoint: 'http://localhost:9080/realms/suuku-domain/protocol/openid-connect/logout',
+  tokenEndpoint: 'http://localhost:9080/realms/suuku-domain/protocol/openid-connect/token',
+  redirectUri: 'http://localhost:5173/',
+  scope: 'profile openid email offline',
+  // Example to redirect back to original path after login has completed
+  // preLogin: () => localStorage.setItem('preLoginPath', window.location.pathname),
+  // postLogin: () => window.location.replace(localStorage.getItem('preLoginPath') || ''),
+  decodeToken: true,
+  autoLogin: false,
+};
+
